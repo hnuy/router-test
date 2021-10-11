2fa73eec181cf0d93ab508dd7f4b3c7c75f4d076
Bhon Bunnag <bhonris@gmail.com>
2021-10-10 21:25:10 -0700

    include indexing

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,5 @@
 import './App.css';
-import { Home } from './pages/Home' 
-import { Contact } from './pages/Contact'
-import { About } from './pages/About/'
+import { Home, Contact, About} from './pages'
 import { BrowserRouter as Router, Route, Switch, Link} from 'react-router-dom'
 function App() {
   return (
